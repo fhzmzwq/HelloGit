b5e1c0191b5b858f35e00a2357dccf9712a2fd5e
w_953 <w_953421@163.com>
2021-07-15 09:19:38 +0800

    更新一

--- HellowGet/src/main/java/com/atguigu/git/HellowGie.java
+++ HellowGet/src/main/java/com/atguigu/git/HellowGie.java
@@ -6,5 +6,6 @@ package com.atguigu.git;
 public class HellowGie {
     public static void main(String[] args) {
         System.out.println("Hellow git");
+        System.out.println("更新一");
     }
 }
