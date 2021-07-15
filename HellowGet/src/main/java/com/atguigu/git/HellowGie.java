11222f1dbb764520a992bcba200a9b1fba3c7577
w_953 <w_953421@163.com>
2021-07-15 09:18:15 +0800

    添加文件

---
+++ HellowGet/src/main/java/com/atguigu/git/HellowGie.java
@@ -0,0 +1,10 @@
+package com.atguigu.git;
+
+/**
+ * Created by zwq on 2021/7/15 9:03
+ */
+public class HellowGie {
+    public static void main(String[] args) {
+        System.out.println("Hellow git");
+    }
+}
